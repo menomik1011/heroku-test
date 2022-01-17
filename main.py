3b6b546cc123dffe55e056603fa1884acf51b627
meno1011 <39692905+meno1011@users.noreply.github.com>
2022-01-17 12:44:09 +0900

    first heroku

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/home')
+def home():
+    return 'Hello, World!'
+
+@app.route('/user/<user_name>/<int:user_id>')
+def user(user_name, user_id):
+    return f'username: {user_name}\n userid: {user_id} !'
+
+@app.route('/api',methods=['POST'])
+def post():
+    value = request.form['input']
+    msg = "%s 님 안녕! " %value
+    return msg
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
